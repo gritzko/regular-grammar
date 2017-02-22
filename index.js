64e5e790ed025842e0e2ed3fe5ebe934704fd758
Victor Grishchenko <victor.grishchenko@gmail.com>
2017-02-22 18:10:46 +0100

    eslint reformatting

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-"use strict";
-
 const Grammar = require('./src/Grammar');
 
 const SWARM_GRAMMAR_RULES = {
 
-    "NUMBER":       /\d+(\.\d+([eE]-?\d+)?)?/,
-    "STRING":       /"(\\.|[^"])*"/,
-    "EMPTY":        "",
-    "BASE":         /[0-9A-Za-z_~]+/,
-    "INT":          /[0-9A-Za-z_~]{1,10}/,
+    NUMBER: /\d+(\.\d+([eE]-?\d+)?)?/,
+    STRING: /"(\\.|[^"])*"/,
+    EMPTY: '',
+    BASE: /[0-9A-Za-z_~]+/,
+    INT: /[0-9A-Za-z_~]{1,10}/,
 
-    "ID":           "INT [+%-]INT?",
-    "SPEC":         "ID? .ID? @ID? :ID?",
-    "CONSTANT":     "NUMBER| STRING| EMPTY|",
-    "VALUE":        "=CONSTANT| >SPEC|",
+    ID: 'INT [+%-]INT?',
+    SPEC: 'ID? .ID? @ID? :ID?',
+    CONSTANT: 'NUMBER| STRING| EMPTY|',
+    VALUE: '=CONSTANT| >SPEC|',
 
-    "RUN":          "ID /BASE?",
-    "CONSTRUN":     "CONSTANT /BASE?",
-    "IDS":          "RUN ,RUN*",
-    "SPECS":        "IDS .IDS? @IDS? :IDS?",
-    "CONSTANTS":    "CONSTRUN ,CONSTRUN*",
-    "BLOCK":        "/INT? IDS? :IDS? =CONSTANTS? >SPECS?",
-    "OPS":          "BLOCK ;BLOCK*",
+    RUN: 'ID /BASE?',
+    CONSTRUN: 'CONSTANT /BASE?',
+    IDS: 'RUN ,RUN*',
+    SPECS: 'IDS .IDS? @IDS? :IDS?',
+    CONSTANTS: 'CONSTRUN ,CONSTRUN*',
+    BLOCK: '/INT? IDS? :IDS? =CONSTANTS? >SPECS?',
+    OPS: 'BLOCK ;BLOCK*',
 
-    "EVENT":        "#SPEC VALUE?",
-    "ON":           "?SPEC VALUE?",
-    "STATE":        "!SPEC |OPS?",
+    EVENT: '#SPEC VALUE?',
+    ON: '?SPEC VALUE?',
+    STATE: '!SPEC |OPS?',
 
-    "FRAME_UP":     "ON* EVENT*",
-    "FRAME_DOWN":   "STATE* EVENT*"
+    FRAME_UP: 'ON* EVENT*',
+    FRAME_DOWN: 'STATE* EVENT*',
 
 };
 
--- src/Grammar.js
+++ src/Grammar.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /** The class generates regex _parsers for simple regular languages
  *  based on grammar rules */
 class Grammar {
@@ -10,10 +8,11 @@ class Grammar {
         // {RULE: regex_str}
         this._parsers = Object.create(null);
         this._patterns = Object.create(null);
-        for(let key in rules)
-            if (rules[key].constructor===RegExp) {
+        for (const key in rules) {
+            if (rules[key].constructor === RegExp) {
                 this._patterns[key] = rules[key].source;
             }
+        }
         this._splitters = Object.create(null);
     }
 
@@ -24,45 +23,44 @@ class Grammar {
         if (!splitter) throw new Error('rule unknown');
         splitter.lastIndex = 0;
         const m = splitter.exec(text);
-        return m && m[0].length===text.length;
+        return m && m[0].length === text.length;
     }
 
     triplets (rule_name) {
-        if (rule_name in this._triplets)
-            return this._triplets[rule_name];
-        const ret = this._triplets[rule_name] = [];
+        if (rule_name in this._triplets) { return this._triplets[rule_name]; }
+        const ret = [];
+        this._triplets[rule_name] = ret;
         const rule = this._rules[rule_name];
-        if (rule===undefined)
-            throw new Error('unknown rule: '+rule_name);
+        if (rule === undefined) { throw new Error('unknown rule: ' + rule_name); }
         Grammar.TRIPLET_RE.lastIndex = 0;
         let m = null;
-        while(m=Grammar.TRIPLET_RE.exec(rule)) {
+        while (m = Grammar.TRIPLET_RE.exec(rule)) {
             const triplet = {
-                formula:    m[0],
-                marker:     m[1]||'',
-                rule:       m[2],
-                quantifier: m[3]||'',
-                repeating:  m[3]==='*' || m[3]==='+'
+                formula: m[0],
+                marker: m[1] || '',
+                rule: m[2],
+                quantifier: m[3] || '',
+                repeating: m[3] === '*' || m[3] === '+',
             };
-            ret.push (triplet);
+            ret.push(triplet);
         }
         return ret;
     }
 
 
     parser (rule_name) {
-        if (rule_name in this._parsers)
-            return this._parsers[rule_name];
+        if (rule_name in this._parsers) { return this._parsers[rule_name]; }
         const pattern = this.pattern(rule_name);
-        return this._parsers[rule_name] = new RegExp('^\\s*'+pattern+'\\s*$', 'm');
+        const re = new RegExp('^\\s*' + pattern + '\\s*$', 'm');
+        this._parsers[rule_name] = re;
+        return re;
     }
 
 
     splitter (triplet) {
         const t = triplet;
-        if (this._splitters[t.formula])
-            return this._splitters[t.formula];
-        let p = (t.marker.length===1 ? '\\':'') + t.marker;
+        if (this._splitters[t.formula]) { return this._splitters[t.formula]; }
+        let p = (t.marker.length === 1 ? '\\' : '') + t.marker;
         p += '(' + this.pattern(t.rule) + ')';
         const splitter = new RegExp(p, 'g');
         this._splitters[t.formula] = splitter;
@@ -71,26 +69,23 @@ class Grammar {
 
 
     pattern (rule_name) {
-        if (rule_name in this._patterns)
-            return this._patterns[rule_name];
+        if (rule_name in this._patterns) { return this._patterns[rule_name]; }
         const triplets = this.triplets(rule_name);
-        const is_chain = triplets.every( t => t.quantifier!=='|' );
-        const pattern = triplets.map( t => {
+        const is_chain = triplets.every(t => t.quantifier !== '|');
+        const pattern = triplets.map((t) => {
             let ret = sterilize(this.pattern(t.rule));
-            if (!t.repeating)
-                ret = '(' + ret + ')';
+            if (!t.repeating) { ret = '(' + ret + ')'; }
             if (t.marker) {
-                ret = (t.marker.length===1 ? '\\':'') + t.marker + ret;
+                ret = (t.marker.length === 1 ? '\\' : '') + t.marker + ret;
             }
-            if (t.quantifier && t.quantifier!=='|') {
+            if (t.quantifier && t.quantifier !== '|') {
                 ret = '(?:' + ret + ')' + t.quantifier;
             }
             if (t.repeating) {
                 ret = '(' + ret + ')';
             }
             return ret;
-
-        } );
+        });
         this._patterns[rule_name] = pattern.join(is_chain ? '\\s*' : '|');
 
         return this._patterns[rule_name];
@@ -107,19 +102,17 @@ class Grammar {
         const triplets = this.triplets(rule_name);
         parser.lastIndex = 0;
         const m = parser.exec(text);
-        if (!m)
-            throw new Error('grammar violation');
-        for(let i=0; i<triplets.length; i++) {
+        if (!m) { throw new Error('grammar violation'); }
+        for (let i = 0; i < triplets.length; i++) {
             const triplet = triplets[i];
-            const match = m[i+1];
+            const match = m[i + 1];
             if (triplet.repeating) {
                 const splitter = this.splitter(triplet);
                 const items = [];
                 ret.push(items);
-                let s=null;
+                let s = null;
                 splitter.lastIndex = 0;
-                while (s=splitter.exec(match))
-                    items.push(s[1]);
+                while (s = splitter.exec(match)) { items.push(s[1]); }
             } else {
                 ret.push(match);
             }
@@ -130,15 +123,15 @@ class Grammar {
     /** test the text against this grammar
      * @returns {Array} an array of rule names this text matches */
     test (text) {
-        return Object.keys(this._rules).filter(rule => this.is(text, rule) );
+        return Object.keys(this._rules).filter(rule => this.is(text, rule));
     }
 
 }
 
-Grammar.TRIPLET_RE =  /(\[.*?\]|[^A-Za-z0-9\s]?)([A-Z][A-Z0-9_]+)([\*\+\?\|]?)/g;
+Grammar.TRIPLET_RE = /(\[.*?\]|[^A-Za-z0-9\s]?)([A-Z][A-Z0-9_]+)([*+?|]?)/g;
 
 function sterilize (pattern) {
-    return pattern.replace(/\((\?\:)?/g, '(?:');
+    return pattern.replace(/\((\?:)?/g, '(?:');
 }
 
 module.exports = Grammar;
