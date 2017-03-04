2e6a2e69c1a105ffe2e3fe7e2ce594c2245ff0c3
Victor Grishchenko <victor.grishchenko@gmail.com>
2017-03-04 15:42:22 +0100

    generic regular lang package

--- test/01_grammar.js
+++ test/01_grammar.js
@@ -1,5 +1,35 @@
-const grammar = require('..');
 const tap = require('tape');
+const Grammar = require('..');
+
+const SWARM_GRAMMAR_RULES = {
+
+    NUMBER: /\d+(\.\d+([eE]-?\d+)?)?/,
+    STRING: /"(\\.|[^"])*"/,
+    EMPTY: '',
+    BASE: /[0-9A-Za-z_~]+/,
+    INT: /[0-9A-Za-z_~]{1,10}/,
+
+    ID: 'INT [+%-]INT?',
+    SPEC: 'ID? .ID? @ID? :ID?',
+    CONSTANT: 'NUMBER| STRING| >ID| EMPTY|',
+
+    RUN: 'INT /BASE? [+%-]INT?',
+    CONSTRUN: 'CONSTANT /BASE?',
+    IDS: 'RUN ,RUN*',
+    CONSTANTS: 'CONSTRUN ,CONSTRUN*',
+    BLOCK: '/INT? IDS? :IDS? =CONSTANTS?',
+    OPS: 'BLOCK ;BLOCK* }EMPTY',
+
+    EVENT: '#ID? .ID? @ID :ID? =CONSTANTS?',
+    ON: '?ID .ID? @ID? :ID? =CONSTANTS?',
+    STATE: '!ID .ID? @ID? :ID? {OPS?',
+
+    FRAME_UP: 'ON* EVENT*',
+    FRAME_DOWN: 'STATE* EVENT*',
+
+};
+
+const grammar = new Grammar(SWARM_GRAMMAR_RULES);
 
 tap('grammar.01.A triplets', (tap) => {
     tap.deepEqual(grammar.triplets('ID'), [
@@ -53,7 +83,7 @@ tap('grammar.01.D grammar.is', (tap) => {
     tap.deepEqual(grammar.test('0').sort().join(), ['INT', 'ID', 'SPEC', 'NUMBER', 'CONSTANT', 'BASE', 'RUN', 'IDS', 'CONSTRUN', 'CONSTANTS', 'BLOCK'].sort().join());
     tap.deepEqual(grammar.test('1.23e4').sort().join(), ['SPEC', 'NUMBER', 'CONSTANT', 'CONSTRUN', 'CONSTANTS'].sort().join());
     tap.deepEqual(grammar.test('12.3E-4').sort().join(), ['SPEC', 'NUMBER', 'CONSTANT', 'CONSTRUN', 'CONSTANTS'].sort().join());
-    tap.deepEqual(grammar.test('\"some string\"').sort().join(), ['STRING', 'CONSTANT', 'CONSTRUN', 'CONSTANTS'].sort().join());
+    tap.deepEqual(grammar.test('"some string"').sort().join(), ['STRING', 'CONSTANT', 'CONSTRUN', 'CONSTANTS'].sort().join());
     tap.deepEqual(grammar.test('0+0').sort().join(), ['ID', 'SPEC', 'RUN', 'IDS', 'BLOCK'].sort().join());
     tap.deepEqual(grammar.test('10-Z0').sort().join(), ['ID', 'SPEC', 'RUN', 'IDS', 'BLOCK'].sort().join());
     tap.deepEqual(grammar.test('Sl0v0').sort().join(), ['INT', 'ID', 'SPEC', 'BASE', 'RUN', 'IDS', 'BLOCK'].sort().join());
